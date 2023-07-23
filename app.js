afa1142c1308d39b2774d0272941d4e680120db4
saeed <mostafazarei13751375@gmail.com>
2023-07-24 01:09:25 +0430

    for

---
+++ app.js
@@ -0,0 +1,34 @@
+
+function sum(array){
+  let num = 0
+  for (let i = 0;i<array.length;i++){
+    num += array[i];
+  }
+   console.log(num);
+
+}
+sum([10,5,90,9,8]);
+
+  function avg(array) {
+      let sum = 0;
+      for (let i = 0; i < array.length; i++) {
+          sum += array[i];
+      }
+      return sum / array.length
+      
+  }
+  console.log(avg([10,5]))
+
+
+  function min(array) {
+
+    console.log( Math.min(...array));
+  }
+  min([10,5])
+  function max(array) {
+
+    console.log( Math.max(...array));
+  }
+  max([10,5])
+
+  
